61afd615b0e69f8519702f01186dd73ba19821cc
Ikenna Udemezue <iykethe1st@gmail.com>
2023-02-15 14:22:52 +0100

    hot fix

--- ethereal-emporium-api-node/index.js
+++ ethereal-emporium-api-node/index.js
@@ -17,9 +17,19 @@ mongoose
 
 app.use(express.json());
 
-// app.use("/", (req, res) => {
-//   res.json("Ethereal Emporium is Live");
-// });
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method == "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+
+  next();
+});
 
 app.use("/api/users", users);
 app.use("/api/auth", auth);
