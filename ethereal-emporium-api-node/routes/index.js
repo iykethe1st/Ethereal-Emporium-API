1a41cbca84a7c6e027d9c102faba45d2989be859
Ikenna Udemezue <iykethe1st@gmail.com>
2023-02-15 11:25:08 +0100

    hot fix

--- ethereal-emporium-api-node/routes/index.js
+++ ethereal-emporium-api-node/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.send("Ethereal Emporium API is Live");
+  res.json("Ethereal Emporium API is Live");
 });
 
 module.exports = router;
