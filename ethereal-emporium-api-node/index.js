164bb4b3cba595a94cd09ee4b0c3e989bfb3ca24
Ikenna Udemezue <iykethe1st@gmail.com>
2023-02-15 11:51:24 +0100

    hot fix

--- ethereal-emporium-api-node/index.js
+++ ethereal-emporium-api-node/index.js
@@ -1,30 +1,11 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
-const users = require("./routes/users");
-const auth = require("./routes/auth");
-const port = 9500;
-const { logger } = require("./logger");
+const port = 9000;
 
-mongoose
-  .connect(
-    "mongodb+srv://admin:admin@cluster0.amghmo9.mongodb.net/?retryWrites=true&w=majority"
-  )
-  .then(() => console.log("Connected to MongoB..."))
-  .catch((err) => console.error("Could not connect to MongoDB...", err));
-
-app.use(express.json());
-
-app.use("/", () => {
-  res.send("Ethereal Emporium is Live");
+app.use("/", (req, res) => {
+  res.json({ message: "AI am Live" });
 });
-
-app.use("/api/users", users);
-app.use("/api/auth", auth);
 
 app.listen(port, () => {
-  logger.info(`Listening on port ${port}...`);
+  console.log("Started server on port " + port);
 });
-
-// export Ethereal_Emporium_Privatekey=mySecureKey
-// export Ethereal_Emporium_Cluster=
