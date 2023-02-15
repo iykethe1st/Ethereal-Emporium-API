0721d596f8283dcfee3991382b5b4ad9f2d12c01
Ikenna Udemezue <iykethe1st@gmail.com>
2023-02-15 10:52:25 +0100

    added logger

--- ethereal-emporium-api-node/index.js
+++ ethereal-emporium-api-node/index.js
@@ -5,7 +5,7 @@ const users = require("./routes/users");
 const auth = require("./routes/auth");
 const index = require("./routes/index");
 const port = 3500;
-const config = require("config");
+const { logger } = require("./logger");
 
 mongoose
   .connect(
@@ -20,7 +20,9 @@ app.use("/api/users", users);
 app.use("/api/auth", auth);
 app.use("/", index);
 
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+app.listen(port, () => {
+  logger.info(`Listening on port ${port}...`);
+});
 
 // export Ethereal_Emporium_Privatekey=mySecureKey
 // export Ethereal_Emporium_Cluster=
